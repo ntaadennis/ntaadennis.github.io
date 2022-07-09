c6993956292fa46f1774186d7852c4a448f5f2f4
ntaadennis <ntaadennis@gmnail.com>
2022-07-09 20:36:07 +0200

    add files

---
+++ index.js
@@ -0,0 +1,33 @@
+function openDropdown() {
+
+    console.log('opening dropdown')
+    console.log(dropdownContainer)
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+}
+
+
+var acc = document.getElementsByClassName("dropdown-trigger");
+var i;
+console.log(acc)
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
